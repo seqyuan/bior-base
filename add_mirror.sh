99e6780d576a94c5055c9047d6b18383f3f4c6c8
seqyuan <ahworld07@gmail.com>
2024-02-18 10:37:58 +0800

    packages

--- add_mirror.sh
+++ add_mirror.sh
@@ -52,3 +52,8 @@ conda install -y -c bioconda bioconductor-complexheatmap
 conda install -y -c bioconda bioconductor-clusterprofiler
 conda install -y -c bioconda bioconductor-ucell
 conda install -y -c bioconda bioconductor-aucell
+conda install -y -c bioconda bioconductor-tfbstools
+conda install -y -c bioconda bioconductor-dirichletmultinomial
+conda install -y -c bioconda r-htmltools=0.5.7
+conda install -y -c conda-forge r-ggiraph
+conda install -y -c r r-ggiraphextra
