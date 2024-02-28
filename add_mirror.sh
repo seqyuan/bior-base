fbc4555a4458886ce2c44b93efeac26a6cb5942c
seqyuan <ahworld07@gmail.com>
2024-02-28 10:17:52 +0800

    wgcna

--- add_mirror.sh
+++ add_mirror.sh
@@ -10,7 +10,7 @@ conda config --add channels bioconda
 
 #echo "options(BioC_mirror='https://mirrors.tuna.tsinghua.edu.cn/bioconductor')" >> ~/.Rprofile
 #echo "options('repos' = c(CRAN='https://mirrors.tuna.tsinghua.edu.cn/CRAN/'))" >> ~/.Rprofile
-
+conda install -y -c bioconda r-wgcna
 conda install -y -c conda-forge r-fitdistrplus
 conda install -y -c conda-forge r-ggridges
 conda install -y -c conda-forge r-tidyverse
